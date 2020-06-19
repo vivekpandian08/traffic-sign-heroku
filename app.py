1c38ffaeee23873a86110041b22120bef26cad6f
Vivekpandian <vivekpandian08@gmail.com>
2020-06-18 20:56:40 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,119 @@
+
+
+
+
+import pandas as pd
+import numpy as np
+
+##Keras
+import tensorflow as tf
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+from PIL import Image
+import os
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+#from gevent.pywsgi import WSGIServer
+
+# Define a flask app
+app = Flask(__name__)
+
+# Model saved with Keras model.save()    
+#MODEL_PATH = 'traffic_classifier.h5'
+
+# Load your trained model
+#model = load_model(MODEL_PATH)
+
+
+#dictionary to label all traffic signs class.
+classes = { 1:'Speed limit (20km/h)',
+            2:'Speed limit (30km/h)', 
+            3:'Speed limit (50km/h)', 
+            4:'Speed limit (60km/h)', 
+            5:'Speed limit (70km/h)', 
+            6:'Speed limit (80km/h)', 
+            7:'End of speed limit (80km/h)', 
+            8:'Speed limit (100km/h)', 
+            9:'Speed limit (120km/h)', 
+            10:'No passing', 
+            11:'No passing veh over 3.5 tons', 
+            12:'Right-of-way at intersection', 
+            13:'Priority road', 
+            14:'Yield', 
+            15:'Stop', 
+            16:'No vehicles', 
+            17:'Veh > 3.5 tons prohibited', 
+            18:'No entry', 
+            19:'General caution', 
+            20:'Dangerous curve left', 
+            21:'Dangerous curve right', 
+            22:'Double curve', 
+            23:'Bumpy road', 
+            24:'Slippery road', 
+            25:'Road narrows on the right', 
+            26:'Road work', 
+            27:'Traffic signals', 
+            28:'Pedestrians', 
+            29:'Children crossing', 
+            30:'Bicycles crossing', 
+            31:'Beware of ice/snow',
+            32:'Wild animals crossing', 
+            33:'End speed + passing limits', 
+            34:'Turn right ahead', 
+            35:'Turn left ahead', 
+            36:'Ahead only', 
+            37:'Go straight or right', 
+            38:'Go straight or left', 
+            39:'Keep right', 
+            40:'Keep left', 
+            41:'Roundabout mandatory', 
+            42:'End of no passing', 
+            43:'End no passing veh > 3.5 tons' }
+
+
+
+
+def read_image(filename):
+    # Load the image
+    img = load_img(filename, target_size=(48, 48))
+    # Convert the image to array
+    img = img_to_array(img)
+    img = img / 255.0
+    # Reshape the image into a sample of 1 channel
+    img =np.expand_dims(img, axis=0)
+    # Prepare it as pixel data
+    #img = img.astype('float32')
+    return img
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+        # read image 
+        image=read_image(file_path)
+        # Make prediction
+        model = load_model( 'model.h5')
+        pred = model.predict_classes([image])[0]
+        sign = classes[pred+1]
+        
+        #print(sign)
+        return sign
+    return None
+	
+if __name__ == '__main__':
+    app.run(debug=True)
